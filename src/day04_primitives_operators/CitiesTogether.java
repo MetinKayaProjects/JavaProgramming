a5d06c3eaa545f946b405d862095fc0b7da93d65
metinkaya1511 <metinkaya1511@gmail.com>
2020-06-19 00:28:00 -0400

    update

--- src/day04_primitives_operators/CitiesTogether.java
+++ src/day04_primitives_operators/CitiesTogether.java
@@ -22,6 +22,7 @@ public class CitiesTogether {
 		String city2="Kyiv";
 		String city3="Moskow";
 		
+		
 		System.out.println("From "+city1+ " to "+city2 + " is $500,");
 		System.out.println("From "+city3+ " to "+city1 + " is $550,");
 		System.out.println("I have been to " +city1+ ", "+city3 + ", " +city2 + ".");
